0a7ddcc0559045d1d89c6f46812791e7f6dc5b50
Nina <frontend-developer@inbox.ru>
2026-01-24 00:19:36 +0300

    Сделала Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const montserrat = Montserrat( {
+  display: "swap",
+  subsets: [ "cyrilic" ],
+} );
 
 export const metadata = {
   title: "Create Next App",
@@ -18,12 +14,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ru">
+    <body
+      className={ montserrat.className }
+    >
+    <main className="bg-[var(--background)] xl:rounded-[60px] overflow-hidden">
+      <Header />
+      { children }
+    </main>
+
+    </body>
     </html>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      // Добавляем кастомные брейкпоинты
+      screens: {
+        "xs": "320px",
+        "sm": "375px",
+        "xl": "1920px",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
